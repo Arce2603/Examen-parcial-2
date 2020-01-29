7bf1897a4bce78d3c243e9e8eaa10ad47b5350d7
Arce2603 <arceliavm98@gmail.com>
2020-01-29 08:47:31 -0600

    parte2

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -3,12 +3,53 @@ let bodyParser = require( 'body-parser' );
 let mongoose = require( 'mongoose' );
 let jsonParser = bodyParser.json();
 let { DATABASE_URL, PORT } = require( './config' );
-
+let bookmarks = require('./model');
 let app = express();
 
 let server;
 
 /* Tu código va aquí */
+app.put('/api/bookmarks/:id', jsonParser, (req, res) => {
+    let id = req.params.id;
+    let id2 = req.body.id;
+    let titulo = req.body.titulo;
+    let descripcion = req.body.descripcion;
+    let obj = {};
+    let url = req.body.url;
+    if (id2 && id2 !== '') {
+        if (id !== id2) {
+            res.statusMessage("id de body y param diferentes")
+            return res.status(409).send();
+        }
+        else {
+            if ((titulo && titulo !== '') || (descripcion && descripcion !== '') || (url && url !== '')) {
+                console.log('modificar');
+                if (titulo !== '')
+                    obj.titulo = titulo;  
+                if (descripcion !== '')
+                    obj.descripcion = descripcion;
+                if (url !== '')
+                    obj.url = url;
+                bookmarks.update(id, obj)
+                    .then(resp => {
+                        return res.status(202).json(resp);
+                    })
+                    .catch (err => {
+                        res.statusMessage(err);
+                        return res.status(400).send();
+                    })
+            }
+            else {
+                res.statusMessage("No contenido a modificar")
+                return res.status(406).send();
+            }
+        }
+    }
+    else {
+        res.statusMessage("id no de body")
+        return res.status(406).send();
+    }
+});
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -1,10 +1,28 @@
 let mongoose = require( 'mongoose' );
 let uuid = require( 'uuid' );
+let bookmarksL = mongoose.model('bookmarks', Schema ({
+    id: uuid.v4(),
+    titulo: String,
+    descripcion: String,
+    url: String
+}));
 
 mongoose.Promise = global.Promise;
 
 /* Tu código va aquí */
+let bookmarkList = {
+    update: function (id, mod) {
+        if (title)
+            bookmarksL.findOneAndUpdate(id, mod, { new: true })
+                .then(response => {
+                    return response;
+                })
+                .catch(err => {
+                    throw Error(err);
+                });
+    }
+};
 
 module.exports = {
-    
+    bookmarkList  
 };
